4a587fa2239c4c9c11b5cf9a241b5a1d9d23aae1
Onkar Sanjay Kodape <onkarko@cybage.com>
2024-10-15 12:27:59 +0530

    added the package

--- test/political_test.py
+++ test/political_test.py
@@ -1,3 +1,14 @@
+import pytest
+import requests
+from unittest.mock import patch
+from pytest_mock import mocker
+
+# Define your API URLs
+API_URL_HOME = "http://127.0.0.1:5002/home"
+API_URL_SPORTS = "http://127.0.0.1:5002/sports"
+API_URL_POLITICAL = "http://127.0.0.1:5002/political"
+
+
 def test_get_home_invalid_route(mocker):
     mocker.patch('requests.get')
     requests.get.return_value.status_code = 404
